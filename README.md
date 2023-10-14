230551772856b3d958ee282a71c82632c2ae09b1
Magnesium- <yyong007@outlook.com>
2023-10-14 12:01:03 +0800

    feat: prisma

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 - trpc
 
+## prisma
+
+- Run prisma db pull to turn your database schema into a Prisma schema.
+- Run prisma generate to generate the Prisma Client. You can then start querying your database.
---
+++ prisma/seed.ts
@@ -0,0 +1,21 @@
+import Prisma from '@prisma/client'
+
+const prisma = new Prisma.PrismaClient()
+
+const seed = async () => {
+  const user = await prisma.uesr.create({
+    data: {
+      name: 'y1',
+      email: 'y1@remix-tRPC.com',
+      profile: {
+        create: {
+          bio: 'http://localhost:3000/bio/xxx.png',
+        }
+      }
+    }
+  })
+
+  console.log("seed user", user);
+}
+
+seed()
